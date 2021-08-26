4cf6fe550c3e69f14dad866c230cd81b3bdd9f30
sikandar1407 <85288583+sikandar1407@users.noreply.github.com>
2021-08-26 15:39:43 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,29 @@
+menu_list_array = ["Veg Margherita Pizza","cheese burst pizza", "Mexican Pizza", "Chicken Tandoori Pizza ", "Chicken Italian Pizza",
+                    "Chicken Nuggets Pizza", "Cheesy Chicken mexican tandoori burst"];
+
+function getmenu(){
+var htmldata="";
+menu_list_array.sort();
+        for(var i=0;i<menu_list_array.length;i++){
+            htmldata=htmldata+ menu_list_array[i] + '<br>'
+        }
+        document.getElementById("display_menu").innerHTML = htmldata;
+        //give the appropriate id name as display_menu  
+}
+
+function add_item(){
+var htmldata;
+var item=document.getElementById("add_item").value;
+        menu_list_array.sort();
+        htmldata=""
+        for(var i=0;i<menu_list_array.length;i++){
+            htmldata=htmldata+imgtags+ menu_list_array[i]+'<br>';
+        }
+         document.getElementById("display_addedmenu").innerHTML = htmldata;		
+      }
+
+function add_top(){
+    var item=document.getElementById("add_item").value;
+    menu_list_array.push(item);
+	add_item();
+}
